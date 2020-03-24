8dd5ec13c3c37deb2bf1444c61957beca069f70a
Phyajom <49375794+Phyajom@users.noreply.github.com>
2020-03-24 09:36:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310295"; ?><br>
+<?php echo "ชื่อ-สกุล นายศุภศักดิ์"; ?><br>
+<iframe width="500" height="500"
+src="https://www.youtube.com/video/s3Kum4lmpF8/edit">
+
+</iframe>
+</BODY>
+</HTML>
